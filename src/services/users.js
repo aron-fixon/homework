72a5b076e02d650e08f4818c9e55ca623d3b3188
Aron <95549768+aron8112@users.noreply.github.com>
2022-08-23 19:20:25 -0300

    Create users.js

---
+++ src/services/users.js
@@ -0,0 +1,9 @@
+const { } = require('../models/user');
+const { } = require('../helpers/httpStatus');
+const { CryptPassword } = require('../utils/encrypter');
+const { } = require('../utils/errorHandler');
+class UsersService {
+    static createNewUser() {
+        encryptPassword
+    }
+}
