57fb77870ef58346f44eca916c124433855c047a
Aron <95549768+aron8112@users.noreply.github.com>
2022-08-23 19:06:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./src/routes/index");
+require("dotenv").config;
+
+//MIDDLEWARES
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTES
+app.get("/", (req, res) => {
+  return res.json({ msg: "Hello, world!" });
+});
+app.use("/homework", indexRouter);
+
+//SERVER
+const port = 3000;
+/*process.env.PORT*/ app.set(3000);
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+/*
+////ERROR HANDLER ----> EVITANDO EL TRY/CATCH
+/////EJEMPLO DE UN ERROR
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+//////EJEMPLO DE COMO FUNCIONARIA EL MIDDLEWARE
+app.use((err, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: "error.message",
+      code: "error.code",
+    },
+  });
+});
+
+const middleware1 = (req, res, next) => {
+  //VALIDACION 1
+};
+const middleware2 = (req, res, next) => {
+  //VALIDACION 2
+};
+*/
